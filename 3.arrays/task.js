37804438f38c12285226c05673b392bdaa5266a9
buzh.61 <buzh.61@gmail.com>
2022-02-20 10:33:12 +0300

    Выполнена фильтрация и преобразование массивов. Мелкие правки кода.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,18 +2,14 @@ function compareArrays(arr1, arr2) {
     let result;
 
     if (arr1.length === arr2.length && arr1.every((elem, index) => arr2[index] === elem)) {
-        result = true;
+        return result = true;
     } else {
-        result = false;
+        return result = false;
     }
-
-    return result;
 }
 
 function advancedFilter(arr) {
-    let resultArr;
-
-    // Ваш код
+    let resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number * 10);
 
     return resultArr; // array
 }
