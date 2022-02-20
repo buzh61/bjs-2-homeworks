ad6509cb25d92095fec3253d826372f7e4220469
buzh.61 <buzh.61@gmail.com>
2022-02-20 14:57:53 +0300

    Объекты: выполнено задание №1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,31 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    this.marks = [...marks];
+}
+
+Student.prototype.getAverage = function () {
+    return this.marks.reduce((acc, num) => acc + num, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
