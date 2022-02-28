47c0bf80af5d9de751b97a7e20bae9e172e8629b
buzh.61 <buzh.61@gmail.com>
2022-02-28 17:39:12 +0300

    Декораторы: выполнено первое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,32 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args){
+        let hash = args.join(',');
+        let idx = cache.findIndex((item) => item["hash"] === hash);
+        
+        if (idx !== -1) {
+            console.log('Из кэша: ' + cache.map(item => item.value));
+            return 'Из кэша: ' + cache.map(item => item.value);
+        } else {
+            
+            let result = func(...args);
+            cache.push({
+                        "hash": hash, 
+                        "value": result,
+                    });
+            
+            if (cache.length > 5) {
+                cache.shift();
+            }
+            
+            console.log('Вычисляем: ' + result);
+            return 'Вычисляем: ' + result;  
+        }
+    }
+
+    return wrapper;
+
 }
 
 
