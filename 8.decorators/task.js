3e882a7c4d6c2b90b4a931f182c044fac1cc0d08
buzh.61 <buzh.61@gmail.com>
2022-03-02 16:33:29 +0300

    Декораторы: подготовка к проверке

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -30,10 +30,50 @@ function cachingDecoratorNew(func) {
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+    let timeout;
+    let flag = false; 
+  
+    function wrapper(...args){
+        if (flag === false){
+            flag = true;
+            timeout = setTimeout(() => {
+                func(...args);
+                flag = false;
+            }, ms);
+
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func(...args), ms);
+
+        }
+
+    }
+    return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+
+function debounceDecorator2(func, ms) {
+    let timeout;
+    let flag = false;
+    
+    function wrapper(...args){
+        let count = 0;
+        console.log(count);
+        if (flag === false){
+            flag = true;
+            timeout = setTimeout(() => {
+                func(...args);
+                count =+ 1;
+                flag = false;
+            }, ms);
+            
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func(...args), ms);
+            
+        }        
+    }
+    return wrapper;
 }
